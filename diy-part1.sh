501e33d0538cb49c12254aacf622ec9503ddada1
EFC631 <78137927+EFC631@users.noreply.github.com>
2021-02-16 18:03:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git https://github.com/coolsnowwolf/lede.git' feeds.conf.default
+#sed -i '$a src-git https://github.com/coolsnowwolf/lede.git' feeds.conf.default
