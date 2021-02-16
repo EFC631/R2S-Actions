18ebabd5c3b9e4d1fcb073ef847a58a001d46ae3
EFC631 <78137927+EFC631@users.noreply.github.com>
2021-02-16 17:54:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git https://github.com/coolsnowwolf/lede.git' feeds.conf.default
